3fdc7b0a660ed481a5564a189eb6fb85b32bb330
Kun Xie <mr.xie.kun@gmail.com>
2017-06-23 20:01:44 -0400

    initial

---
+++ js/controller/doccontroller.js
@@ -0,0 +1,31 @@
+/**
+ * Created by KJXie on 2017/6/23.
+ */
+var myApp = angular.module('myApp', []);
+myApp.controller('DocCtrl', function($scope){
+    $scope.paragraphs = [];
+    $scope.formContent = "";
+    $scope.formHead = "";
+    $scope.totalParagraph = $scope.paragraphs.length;
+
+    $scope.formReset = function(){
+        $scope.formContent = "";
+        $scope.formHead = "";
+    };
+
+    $scope.formSubmit = function(){
+        var content = $scope.formContent;
+        var param = {
+            content: content
+        };
+        $.ajax({
+            url:'xiekun.xyz/api/document',
+            type:'post',
+            dataType:'json',
+            success: function(data) {
+
+            },
+            data: param
+        });
+    }
+});
